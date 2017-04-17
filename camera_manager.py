fc4587a9216739c1e6c6bcd16b42e2160029d575
jaakko <jaakko.jokinen@outlook.com>
2017-04-17 23:23:58 +0300

    Only init camera once

--- camera_manager.py
+++ camera_manager.py
@@ -9,14 +9,14 @@ from temperature_manager import TEMP_manager
 class Camera_manager:
     def __init__(self):
         self.temp = TEMP_manager()
+        self.camera = picamera.PiCamera()
 
     def take_image(self):
-        camera = picamera.PiCamera()
-        camera.resolution = (1024, 768)
-        camera.start_preview()
+        self.camera.resolution = (1024, 768)
+        self.camera.start_preview()
         # Camera warm-up time
         time.sleep(2)
-        camera.annotate_background = picamera.Color('black')
+        self.camera.annotate_background = picamera.Color('black')
         byte_string = str(self.temp.get_temp_as_celsius()) + '(arvioitu tavoitelampotila saavutetaan' + str(self.temp.estimate_time()) + 'kuluttua)'
-        camera.annotate_text = byte_string.decode('utf-8')
-        camera.capture('source.png')
+        self.camera.annotate_text = byte_string.decode('utf-8')
+        self.camera.capture('source.png')
